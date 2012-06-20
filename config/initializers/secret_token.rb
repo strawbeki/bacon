a46ce889c11a90cef9af21155b93136ab4dea255
Beki Collins <strawbeki@gmail.com>
2012-06-20 17:31:02 +0100

    set up git, initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bacon::Application.config.secret_token = 'e5b32827cd345ab353a1d6bc5dff885584d8bf2aa27bfeb794f6a03a4ef007f4a0940b748839fa585aa61712d406c395f2d208847d399f92827114ab27c2b175'
